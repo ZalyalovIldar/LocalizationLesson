124005a5a775fb42e31305436b7d6f2ac5e2ef31
Ильда  Залялов <IldarZalyalov@MacBook-Pro-Ilda.local>
2015-12-25 14:59:00 +0300

    init commit

---
+++ LocalizationProject/LocalizationProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocalizationProject
+//
+//  Created by Ильда  Залялов on 25.12.15.
+//  Copyright © 2015 Ильда  Залялов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocalizationProject/LocalizationProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocalizationProject
+//
+//  Created by Ильда  Залялов on 25.12.15.
+//  Copyright © 2015 Ильда  Залялов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
